35d62e45c577fc0dd27292172bceb2f9c447c305
JiaLong Wang <rdltspg77@163.com>
2017-09-06 17:39:22 +0800

    Commit chapter05 code for python

---
+++ ch05/test.py
@@ -0,0 +1,12 @@
+__all__ = ["Error", "encode", "decode"] #export these only
+
+# import module1
+# module1.printer('Hello World!')
+
+# from module1 import printer
+# printer('Hello world!')
+
+# from module1 import *
+# printer('Hello world!')
+
+import module2
